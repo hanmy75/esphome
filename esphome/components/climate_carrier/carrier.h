5a7c8fe8393c8ee5dc334fcf098999d568fcf049
MY Han <han.my75@gmail.com>
2020-10-06 07:50:59 +0100

    Support fan speed and swing mode

--- esphome/components/climate_carrier/carrier.h
+++ esphome/components/climate_carrier/carrier.h
@@ -11,7 +11,11 @@ const uint8_t CARRIER_TEMP_MAX = 30;  // Celsius
 
 class CarrierClimate : public climate_ir::ClimateIR {
  public:
-  CarrierClimate() : climate_ir::ClimateIR(CARRIER_TEMP_MIN, CARRIER_TEMP_MAX) {}
+  CarrierClimate() : climate_ir::ClimateIR(CARRIER_TEMP_MIN, CARRIER_TEMP_MAX, 1.0f, true, true,
+                              {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
+                               climate::CLIMATE_FAN_HIGH},
+                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH, climate::CLIMATE_SWING_VERTICAL, 
+                               climate::CLIMATE_SWING_HORIZONTAL}) {}
   void set_threshold(uint8_t threshold);
 
  protected:
